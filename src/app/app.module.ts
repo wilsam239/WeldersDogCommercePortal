07a0f205864004ce8bb1fec8b563825b328c689b
wilsam239 <swilliamson239@hotmail.com>
2021-09-04 23:11:51 +1000

    Started working on WooCommerce api responses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthComponent } from './auth/auth.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-export const WCAPI = require('@woocommerce/woocommerce-rest-api')
+import WooCommerceRestApi from '@woocommerce/woocommerce-rest-api';
+
+export const WooCommerce = new WooCommerceRestApi({
+  url:"https://theweldersdog.com.au",
+  consumerKey: 'ck_9036775c5b6869a39e3d087f106b94608523caa3',
+  consumerSecret: 'cs_e6c49401eaa1a4b4f9d6f3a1d22260ffa4140688',
+  version: 'wc/v3'
+})
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { WooCommerce } from '../app.module';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
-
   constructor() {
+    WooCommerce.get('')
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
